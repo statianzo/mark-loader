0ab61e47924ef0b5f3e9a8787ae2b5c6c52f4fbc
Jason Staten <jason.staten@domo.com>
2017-01-26 11:09:57 -0700

    first

---
+++ index.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+const startMark = (markName) => (
+  `;global.performance && global.performance.mark && global.performance.mark('${markName}_start');`
+);
+
+const endMark = (markName) => (
+  `;global.performance && global.performance.mark && global.performance.mark('${markName}_end');`
+);
+
+const measure = (markName) => (
+  `;global.performance && global.performance.measure && global.performance.measure('${markName}_init', '${markName}_start', '${markName}_end');`
+);
+
+const markLoader = function (content) {
+  const context = this._compiler.context || process.cwd();
+  const resourcePath = this.resourcePath;
+  const markName = path.relative(context, resourcePath).replace(/[^a-zA-Z0-9]/g, '_');
+
+  return [
+    startMark(markName),
+    content,
+    endMark(markName),
+    measure(markName),
+  ].join('');
+};
+
+module.exports = markLoader;
